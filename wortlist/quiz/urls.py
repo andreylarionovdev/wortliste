13cee9290f7ff152a50fd491abac67602f93dc8f
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-09-10 23:37:10 +0200

    add check word sentence action

--- wortlist/quiz/urls.py
+++ wortlist/quiz/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:word_id>/', views.detail, name='detail')
+    path('<int:word_id>/', views.word_detail, name='word_detail'),
+    path('<int:word_id>/check/', views.check_word, name='check_word')
 ]
--- wortlist/quiz/views.py
+++ wortlist/quiz/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.db.models.functions import Lower
+from django.urls import reverse
 from .models import Word, Sentence
 
 
@@ -8,10 +9,32 @@ def index(request):
     return render(request, 'quiz/index.html', {'words': words})
 
 
-def detail(request, word_id):
+def word_detail(request, word_id):
     word = get_object_or_404(Word, pk=word_id)
     sentences = Sentence.objects.filter(word=word)
     context = {'word': word, 'sentences': sentences}
     if word.is_verb():
         context['verb_forms'] = Word.objects.filter(verb_inf=word)
-    return render(request, 'quiz/detail.html', context)
+    return render(request, 'quiz/word-detail.html', context)
+
+
+def check_word(request, word_id):
+    word = get_object_or_404(Word, pk=word_id)
+    context = {'word': word}
+    try:
+        Sentence.objects.get(word=word, text=request.POST['sentence'])
+    except KeyError:
+        return render(request, 'quiz/check-word.html', context)
+    except Sentence.DoesNotExist:
+        context['error_message'] = 'Try again!'
+        return render(request, 'quiz/check-word.html', context)
+    else:
+        next_word = get_next_word_for_check(word)
+        return HttpResponseRedirect(reverse('check_word', args=(next_word.id,)))
+
+
+def get_next_word_for_check(word):
+    word = Word.objects.filter(verb_inf=None, id__gt=word.id).first()
+    if not word:
+        return Word.objects.filter(verb_inf=None).first()
+    return word
